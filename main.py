a717dbb44f78ae9cba0b1ad183feca8638dd6556
Sanya787 <114693100+Sanya787@users.noreply.github.com>
2023-02-06 19:30:59 +0300

    Initial search version

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
-from PyQt5 import uic, QtCore
+from io import BytesIO
+import sys
+import requests
+from PIL import Image
+from PyQt5 import uic
+from PyQt5 import QtCore
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QDialog, QLabel, QPushButton, QLineEdit,
     QComboBox, QRadioButton, QTextEdit
 )
 
-import sys
+toponym_to_find = 'Москва'
+
+
+def search(name='Москва, кремль'):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "69112cab-042a-47a0-b2c3-b53694ca4271",
+        "geocode": name,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        pass
+
+    json_response = response.json()
+
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+
+    return toponym["Point"]["pos"]
+
+
+def get_image(coords, delta="0.005", map='map'): # coords: "coords_x coords_y"
+    toponym_longitude, toponym_lattitude = coords.split(" ")
+
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": map,
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    return BytesIO(response.content)
 
 
 class MainWin(QMainWindow):
@@ -24,9 +65,11 @@ class MainWin(QMainWindow):
             self.post_index = True
         address = self.adress_edit.toPlainText()
         map_type = self.comboBox.currentText()
+        Image.open(get_image(search(address), map=map_type)).show()
 
     def clean_map(self):
         pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
